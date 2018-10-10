fcc7c1cd235532ff08bced6b43d5d68bfb760b61
John Ky <newhoggy@gmail.com>
2018-10-11 07:55:02 +1100

    Benchmark map fusion

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,21 +6,26 @@ module Main where
 import Criterion.Main
 import Data.Word
 
+import qualified Baseline.Data.Stream.Vector.Storable     as USDVS
 import qualified Data.Vector.Storable                     as DVS
 import qualified HaskellWorks.Data.Stream.Vector.Storable as SDVS
 
 mkEnv :: IO (DVS.Vector Word64)
 mkEnv = do
-  let !v = DVS.replicate 1000 (0 :: Word64)
+  let !v = DVS.replicate 100000 (0 :: Word64)
   return v
 
-foo :: DVS.Vector Word64 -> DVS.Vector Word64
-foo v = SDVS.map (+123) (SDVS.map (+123) v)
+runMap :: DVS.Vector Word64 -> DVS.Vector Word64
+runMap = SDVS.map (+123) . SDVS.map (+123) . SDVS.map (+123)
+
+runMapUnfused :: DVS.Vector Word64 -> DVS.Vector Word64
+runMapUnfused = USDVS.map (+123) . USDVS.map (+123) . USDVS.map (+123)
 
 benchThings :: [Benchmark]
 benchThings =
   [ env mkEnv $ \v -> bgroup "mkEnv"
-    [ bench "xxxx" (whnf foo v)
+    [ bench "DVS.map"         (whnf runMap        v)
+    , bench "DVS.mapUnfused"  (whnf runMapUnfused v)
     ]
   ]
 
