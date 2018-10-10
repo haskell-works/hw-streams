32f7f7476dcbac177984cd2c5bf29f0b515ac9f2
John Ky <newhoggy@gmail.com>
2018-10-10 20:27:50 +1100

    New map function.  Support for non-storable vectors

---
+++ src/HaskellWorks/Data/Stream/Vector.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module HaskellWorks.Data.Stream.Vector where
+
+import Control.Monad.ST
+import HaskellWorks.Data.Stream
+
+import qualified Data.Vector              as DV
+import qualified Data.Vector.Mutable      as DVM
+import qualified HaskellWorks.Data.Stream as HW
+
+unstream :: forall a. HW.Stream a -> DV.Vector a
+unstream (HW.Stream step initialState n) = runST $ do
+  v <- DVM.unsafeNew n
+  loop step v 0 initialState
+  where loop :: (s -> Step s a) -> DVM.MVector t a -> Int -> s -> ST t (DV.Vector a)
+        loop g v i s = case g s of
+          Yield a s' -> do
+            DVM.unsafeWrite v i a
+            loop g v (i + 1) s'
+          Skip s0 -> loop g v i s0
+          Done -> DV.freeze v
+
+stream :: forall a. DV.Vector a -> Stream a
+stream v = Stream step 0 len
+  where len = DV.length v
+        step i = if i >= len
+          then Done
+          else Yield (DV.unsafeIndex v i) (i + 1)
+
+map :: (a -> b) -> DV.Vector a -> DV.Vector b
+map f = unstream . fmap f . stream
