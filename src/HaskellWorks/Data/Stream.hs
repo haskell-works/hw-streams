a57a67985ca30d32dc9336e5011c9c72921b6a12
John Ky <newhoggy@gmail.com>
2018-10-10 19:30:34 +1100

    Functor instances

--- src/HaskellWorks/Data/Stream.hs
+++ src/HaskellWorks/Data/Stream.hs
@@ -9,7 +9,17 @@ data Stream a where
     -> Int
     -> Stream a
 
+instance Functor Stream where
+  fmap f (Stream step s i) = Stream (fmap f <$> step) s i
+  {-# INLINE fmap #-}
+
 data Step s a
   = Yield a s
   | Skip s
   | Done
+
+instance Functor (Step s) where
+  fmap f (Yield a s) = Yield (f a) s
+  fmap _ (Skip s)    = Skip s
+  fmap _ Done        = Done
+  {-# INLINE fmap #-}
