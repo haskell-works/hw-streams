473b75821dd61d4036b8a74b320597dc3ef82dc9
John Ky <newhoggy@gmail.com>
2018-10-13 13:47:57 +1100

    Benchmarks for dotp

--- bench/Main.hs
+++ bench/Main.hs
@@ -11,6 +11,7 @@ import qualified Baseline.Data.Stream.Vector.Storable     as USDVS
 import qualified Data.Vector.Storable                     as DVS
 import qualified HaskellWorks.Data.Stream                 as S
 import qualified HaskellWorks.Data.Stream.Vector.Storable as SDVS
+import qualified Reference.Data.Stream.Vector.Storable    as DVS
 
 mkReplicateVector :: IO (DVS.Vector Word64)
 mkReplicateVector = do
@@ -21,6 +22,12 @@ mkEnumVector :: IO (DVS.Vector Word64)
 mkEnumVector = do
   let !v = SDVS.enumFromStepN 0 1 100000
   return v
+
+mkEnumVector2 :: IO (DVS.Vector Word64, DVS.Vector Word64)
+mkEnumVector2 = do
+  let !v = SDVS.enumFromStepN 0 1 100000
+  let !w = SDVS.enumFromStepN 0 1 100000
+  return (v, w)
 
 sumall :: DVS.Vector Word64 -> Word64
 sumall = S.foldl (+) 123 . SDVS.stream . SDVS.map (+1) . SDVS.map (+1) . SDVS.map (+1)
@@ -34,18 +41,23 @@ runMapUnfused = USDVS.map (+123) . USDVS.map (+123) . USDVS.map (+123)
 benchThings :: [Benchmark]
 benchThings = join $
   [ [ env mkReplicateVector $ \v -> bgroup "mkReplicateVector"
-      [ bench "DVS.map"         (whnf runMap          v)
-      , bench "DVS.mapUnfused"  (whnf runMapUnfused   v)
+      [ bench "DVS.map"         (whnf runMap              v)
+      , bench "DVS.mapUnfused"  (whnf runMapUnfused       v)
       ]
     ]
   , [ env mkEnumVector $ \v -> bgroup "mkEnumVector"
-      [ bench "DVS.map"         (whnf runMap          v)
-      , bench "DVS.mapUnfused"  (whnf runMapUnfused   v)
-      , bench "DVS.foldl"       (whnf sumall          v)
+      [ bench "DVS.map"         (whnf runMap              v)
+      , bench "DVS.mapUnfused"  (whnf runMapUnfused       v)
+      , bench "DVS.sumall"      (whnf sumall              v)
+      , bench "DVS.foldl"       (whnf (SDVS.foldl (+) 0)  v)
+      ]
+    ]
+  , [ env mkEnumVector2 $ \ ~(v, w) -> bgroup "mkEnumVector2"
+      [ bench "SDVS.dotp"        (whnf (SDVS.dotp v)       w)
+      , bench "DVS.dotp"         (whnf (DVS.dotp v)       w)
       ]
     ]
   ]
 
 main :: IO ()
 main = defaultMain benchThings
-
