9942959576d4a32b34d884a13524eaa326551501
John Ky <newhoggy@gmail.com>
2018-10-20 01:20:05 -0700

    Better formulation of Chunk

--- src/HaskellWorks/Data/Stream/Chunk.hs
+++ src/HaskellWorks/Data/Stream/Chunk.hs
@@ -2,11 +2,11 @@
 
 module HaskellWorks.Data.Stream.Chunk where
 
-import Control.Monad.ST
+import Control.Monad.Primitive
 
 import qualified Data.Vector.Storable as DVS
 
 data Chunk a = Chunk
   { chunkSize  :: Int
-  , chunkWrite :: forall s. DVS.MVector s a -> ST s ()
+  , chunkWrite :: forall m. PrimMonad m => DVS.MVector (PrimState m) a -> m ()
   }
