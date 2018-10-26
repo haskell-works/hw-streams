24bee69c99c20320b9fd5e792321389f9fd7d566
John Ky <newhoggy@gmail.com>
2018-10-26 15:17:38 +1100

    New ops functions: bitwiseShiftDown, bitwiseXor, bitwiseOr, bitwiseAnd. New stream functions: append, drop

--- src/HaskellWorks/Data/Streams/Stream.hs
+++ src/HaskellWorks/Data/Streams/Stream.hs
@@ -3,7 +3,9 @@
 
 module HaskellWorks.Data.Streams.Stream where
 
-import Prelude hiding (zipWith)
+import Data.Bool
+
+import Prelude hiding (drop, zipWith)
 
 data Stream a where
   Stream :: ()
@@ -42,13 +44,13 @@ zipWith f (Stream stepa sa na) (Stream stepb sb nb) = Stream step (sa, sb, Nothi
 
 zipWithState :: (a -> b -> s -> (c, s)) -> s -> Stream a -> Stream b -> Stream c
 zipWithState f state (Stream stepa sa na) (Stream stepb sb nb) = Stream step (sa, sb, Nothing, state) (min na nb)
-  where step (ta, tb, Nothing, state) = case stepa ta of
-          Yield xa ta0 -> Skip (ta0, tb, Just xa, state)
-          Skip ta0     -> Skip (ta0, tb, Nothing, state)
+  where step (ta, tb, Nothing, oldState) = case stepa ta of
+          Yield xa ta0 -> Skip (ta0, tb, Just xa, oldState)
+          Skip ta0     -> Skip (ta0, tb, Nothing, oldState)
           Done         -> Done
-        step (ta, tb, Just xa, state) = case stepb tb of
+        step (ta, tb, Just xa, oldState) = case stepb tb of
           Yield y tb0 -> let (newValue, newState) = f xa y state in Yield newValue (ta, tb0, Nothing, newState)
-          Skip tb0    -> Skip (ta, tb0, Just xa, state)
+          Skip tb0    -> Skip (ta, tb0, Just xa, oldState)
           Done        -> Done
         {-# INLINE [0] step #-}
 {-# INLINE [1] zipWithState #-}
@@ -68,5 +70,29 @@ foldl f z (Stream step s _) = loop z s
           Done       -> za
 {-# INLINE [1] foldl #-}
 
+drop :: Int -> Stream a -> Stream a
+drop n s@(Stream step state size) = if n > 0
+  then case step state of
+    Yield _ newState -> drop (n - 1)  $ Stream step newState (size - 1)
+    Skip newState    -> drop  n       $ Stream step newState size
+    Done             -> s
+  else s
+
 {-# RULES
   "zipWith xs xs [Vector.Stream]" forall f xs. zipWith f xs xs = fmap (\x -> f x x) xs   #-}
+
+append :: Stream a -> Stream a -> Stream a
+append (Stream stepa ta na) (Stream stepb tb nb) = Stream step (Left ta) (na + nb)
+  where step (Left sa) = case stepa sa of
+          Yield x sa' -> Yield x (Left  sa')
+          Skip    sa' -> Skip    (Left  sa')
+          Done        -> Skip    (Right tb)
+        step (Right sb) = case stepb sb of
+          Yield x sb' -> Yield x (Right sb')
+          Skip    sb' -> Skip    (Right sb')
+          Done        -> Done
+        {-# INLINE step #-}
+{-# INLINE append #-}
+
+singleton :: a -> Stream a
+singleton a = Stream (bool Done (Yield a False)) True 1
--- src/HaskellWorks/Data/Streams/Stream/Ops.hs
+++ src/HaskellWorks/Data/Streams/Stream/Ops.hs
@@ -6,6 +6,8 @@ import Data.Word
 import GHC.Int
 import GHC.Prim
 import GHC.Word                         hiding (ltWord)
+import HaskellWorks.Data.Bits.BitWise   ((.&.), (.<.), (.>.), (.^.), (.|.))
+import HaskellWorks.Data.Positioning
 import HaskellWorks.Data.Streams.Stream
 
 import qualified HaskellWorks.Data.Bits.BitWise   as BW
@@ -17,6 +19,20 @@ ltWord (W64# a#) (W64# b#) = fromIntegral (I64# (ltWord# a# b#))
 
 comp :: Stream Word64 -> Stream Word64
 comp = fmap BW.comp
+
+bitwiseAnd :: Stream Word64 -> Stream Word64 -> Stream Word64
+bitwiseAnd = HW.zipWith (.&.)
+
+bitwiseOr :: Stream Word64 -> Stream Word64 -> Stream Word64
+bitwiseOr = HW.zipWith (.|.)
+
+bitwiseXor :: Stream Word64 -> Stream Word64 -> Stream Word64
+bitwiseXor = HW.zipWith (.^.)
+
+bitwiseShiftDown :: Count -> Stream Word64 -> Stream Word64
+bitwiseShiftDown n as = HW.zipWith splice as (HW.drop 1 as `append` HW.singleton 0)
+  where splice :: Word64 -> Word64 -> Word64
+        splice a b = (a .>. n) .|. (b .<. (64 - n))
 
 add :: Stream Word64 -> Stream Word64 -> Stream Word64
 add = HW.zipWithState (\a b c -> let d = a + b in (c + d, d `ltWord` a)) 0
